1822898f0c508f4a99370be2d5f9aab32fadf462
Tanmayi Sai <saitanmayi@gmail.com>
2020-05-22 23:55:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
+import plotly.graph_objs as go
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash()
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/' +
+    '5d1ea79569ed194d432e56108a04d188/raw/' +
+    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
+    'gdp-life-exp-2007.csv')
 
 
 app.layout = html.Div([
@@ -15,12 +18,12 @@ app.layout = html.Div([
         id='life-exp-vs-gdp',
         figure={
             'data': [
-                dict(
+                go.Scatter(
                     x=df[df['continent'] == i]['gdp per capita'],
                     y=df[df['continent'] == i]['life expectancy'],
                     text=df[df['continent'] == i]['country'],
                     mode='markers',
-                    opacity=0.7,
+                    opacity=0.8,
                     marker={
                         'size': 15,
                         'line': {'width': 0.5, 'color': 'white'}
@@ -28,7 +31,7 @@ app.layout = html.Div([
                     name=i
                 ) for i in df.continent.unique()
             ],
-            'layout': dict(
+            'layout': go.Layout(
                 xaxis={'type': 'log', 'title': 'GDP Per Capita'},
                 yaxis={'title': 'Life Expectancy'},
                 margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
@@ -40,4 +43,4 @@ app.layout = html.Div([
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
